645546657efa0ee0d22f7906a97640aed884e505
Jared Carrano <jcarrano@newvisions.org>
2016-06-05 18:35:23 -0400

    allows copies to be passed as query param

--- app.js
+++ app.js
@@ -1,51 +1,59 @@
 'use strict';
 
 let http = require('http');
+let url = require('url') ;
 const PORT = 8080;
 
-var fs = require('fs');
-var pdf = require('html-pdf');
-var dust = require('dustjs-helpers');
-var _ = require('lodash');
-var NUM_STUDENT_COPIES = 1;
-
+let fs = require('fs');
+let pdf = require('html-pdf');
+let dust = require('dustjs-helpers');
+let _ = require('lodash');
+let studentFixture = fs.readFileSync('./fixtures/student.json', 'utf8');
+const NUM_STUDENT_COPIES = 1;
 
 http.createServer((request, response) => {
 
-  generateProfiles((err, stream) => {
+  let startTime = new Date().getTime();
+
+  let params = url.parse(request.url, true).query;
+  let numberOfCopies = params.copies || 1;
+
+  console.log('Number of copies to make:', numberOfCopies);
+
+  let studentFixtures = makeCopies(JSON.parse(studentFixture), numberOfCopies);
+
+  //Pre-compile (via gulp) and render precompiled file at runtime
+  let compiled = fs.readFileSync('./dist/profile.js', 'utf8');
+  dust.loadSource(compiled);
+
+  let data = {
+    students: studentFixtures
+  };
+
+  generateProfiles(data, (err, stream) => {
     if (err) console.error(err);
 
-    // res.setHeader('Content-disposition', 'attachment; filename=Student_Profile');
-    // res.setHeader('Content-type', 'application/pdf');
+    response.setHeader('Content-disposition', 'attachment; filename=Student_Profile');
+    response.setHeader('Content-type', 'application/pdf');
 
     stream
-      .pipe(fs.createWriteStream('./output/studentProfile.pdf'))
+      .pipe(response)
       .on('finish', () => {
-        console.log('pdf is now ready');
-        response.end('It Works!! Path Hit: ' + request.url);
+        let endTime = new Date().getTime();
+        let elapsed = (endTime - startTime) / 1000;
+        console.log('Elapsed time', elapsed, 'seconds');
       });
   });
 }).listen(PORT, function () {
   console.log('Server listening on Port', PORT);
 }); // Activates this server, listening on port 8080.
 
-var studentFixture = fs.readFileSync('./fixtures/student.json', 'utf8');
-var studentFixtures = makeCopies(JSON.parse(studentFixture), NUM_STUDENT_COPIES);
 
-//Pre-compile (via gulp) and render precompiled file at runtime
-var compiled = fs.readFileSync('./dist/profile.js', 'utf8');
-dust.loadSource(compiled);
-
-var data = {
-  students: studentFixtures
-};
-
-function generateProfiles(callback) {
-  console.log('Generating profile...')
+function generateProfiles(data, callback) {
   // Insert data into the template
   dust.render('profile', data, function (err, out) {
     if (err) return callback(err);
-    var options = {
+    let options = {
       format: 'Letter',
       timeout: 60000,
       border: {
@@ -59,7 +67,6 @@ function generateProfiles(callback) {
         contents: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>'
       }
     };
-
     // Converts HTML to PDF and returns a stream
     pdf.create(out, options).toStream(callback);
   });
@@ -68,8 +75,8 @@ function generateProfiles(callback) {
 
 
 function makeCopies(studentFixture, numberOfCopies) {
-  var copies = [];
-  for (var i = 0; i < numberOfCopies; i++) {
+  let copies = [];
+  for (let i = 0; i < numberOfCopies; i++) {
     copies.push(_.cloneDeep(studentFixture));
   }
   return copies;
